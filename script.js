12a96612e6ff83d117649fc2502d71971f08c888
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-11-27 17:10:39 -0500

    refactoring done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("checking connection");
+let csvData = ("ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor\'s Assistant,26")
+
+let rows = csvData.split("\n");
+console.log(rows);
+
+let headers = rows[0].split(",")
+console.log(headers); //this is a string
+
+// let csvDataHeaders = rows.splice(0,1)
+// console.log(csvDataNoHeaders); //this is an array 
+
+let csvDataTable = []
+
+for (let i = 1; i < rows.length; i++) {
+    let cells = rows[i].split(',');
+  
+    // Create an object to store the current row's data
+    let rowData = {};
+  
+    // Loop through headers and assign values to corresponding keys in the object
+    for (let j = 0; j < headers.length; j++) {
+      rowData[headers[j]] = cells[j];
+    }
+    csvDataTable.push(rowData);
+}
+console.log(csvDataTable);
